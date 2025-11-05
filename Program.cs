bd4e07f7ff9f460010dcfe51937c4be055e8c3a3
c3hmke <cehmke91@gmail.com>
2025-11-05 16:22:25 +0100

    Import static SDL member

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
-using SDL2;
 using System.Runtime.InteropServices;
+using static SDL2.SDL;
 
 namespace CHIP_8;
 
@@ -8,11 +8,11 @@ public static class Program
 {
     static void Main(string[] args)
     {
-        if (SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) < 0) throw new Exception("SDL init failed");
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0) throw new Exception("SDL init failed");
 
         CHIP8 chip8 = new();
-        nint window = SDL.SDL_CreateWindow("CHIP-8", 128, 128, 64 * 8, 32 * 8, 0);
-        nint renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+        nint window = SDL_CreateWindow("CHIP-8", 128, 128, 64 * 8, 32 * 8, 0);
+        nint renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
 
         using (BinaryReader reader = new(new FileStream("../../../ROMs/BRIX", FileMode.Open)))
         {
@@ -26,12 +26,12 @@ public static class Program
         }
 
         int sample = 0, beeps = 0;
-        SDL.SDL_AudioSpec audioSpec = new()
+        SDL_AudioSpec audioSpec = new()
         {
             channels = 1,
             freq     = 44100,
             samples  = 256,
-            format   = SDL.AUDIO_S8,
+            format   = AUDIO_S8,
             callback = (_, stream, length) =>
             {
                 var waveData = new sbyte[length];
@@ -50,12 +50,12 @@ public static class Program
             }
         };
 
-        SDL.SDL_OpenAudio(ref audioSpec, 0);
-        SDL.SDL_PauseAudio(0);
+        SDL_OpenAudio(ref audioSpec, 0);
+        SDL_PauseAudio(0);
         
-        nint SDLTexture = SDL.SDL_CreateTexture(
-            renderer, SDL.SDL_PIXELFORMAT_RGBA8888,
-            (int) SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING,
+        nint SDLTexture = SDL_CreateTexture(
+            renderer, SDL_PIXELFORMAT_RGBA8888,
+            (int) SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING,
             64, 32
         );
         
@@ -80,17 +80,17 @@ public static class Program
             accumulator += elapsed;
 
             // Handle input outside of the 60Hz rate
-            while (SDL.SDL_PollEvent(out SDL.SDL_Event e) != 0)
+            while (SDL_PollEvent(out SDL_Event e) != 0)
             {
                 int key = keycodeToIndex(e.key.keysym.sym);
                 switch (e.type)
                 {
-                    case SDL.SDL_EventType.SDL_QUIT: running = false; break;
-                    case SDL.SDL_EventType.SDL_KEYDOWN:
+                    case SDL_EventType.SDL_QUIT: running = false; break;
+                    case SDL_EventType.SDL_KEYDOWN:
                         chip8.Keyboard |= (ushort)(1 << key);
                         if (chip8.WaitingForKeyPress) chip8.KeyPressed((byte)key);
                         break;
-                    case SDL.SDL_EventType.SDL_KEYUP:
+                    case SDL_EventType.SDL_KEYUP:
                         chip8.Keyboard &= (ushort)~(1 << key);
                         break;
                 }
@@ -102,7 +102,7 @@ public static class Program
                 GCHandle displayHandle = GCHandle.Alloc(chip8.Display, GCHandleType.Pinned);
                 try
                 {
-                    SDL.SDL_UpdateTexture(SDLTexture, IntPtr.Zero, displayHandle.AddrOfPinnedObject(), 64 * 4);
+                    SDL_UpdateTexture(SDLTexture, IntPtr.Zero, displayHandle.AddrOfPinnedObject(), 64 * 4);
                 }
                 finally
                 {
@@ -110,19 +110,19 @@ public static class Program
                 }
 
                 // Clear -> Copy -> Present
-                SDL.SDL_RenderClear(renderer);
-                SDL.SDL_RenderCopy(renderer, SDLTexture, IntPtr.Zero, IntPtr.Zero);
-                SDL.SDL_RenderPresent(renderer);
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, SDLTexture, IntPtr.Zero, IntPtr.Zero);
+                SDL_RenderPresent(renderer);
 
                 accumulator -= frameTicks;
             }
         }
         
-        SDL.SDL_DestroyRenderer(renderer);
-        SDL.SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
     }
 
-    private static int keycodeToIndex(SDL.SDL_Keycode keycode)
+    private static int keycodeToIndex(SDL_Keycode keycode)
     {
         var key  = (int)keycode;                    // ascii int value for the key pressed
         return (key < 58) ? key - 48 : key - 87;    // index for that key
