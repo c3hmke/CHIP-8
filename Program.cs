b6b9f1ff7706e342816dc1cafaa15ee66010d992
c3hmke <cehmke91@gmail.com>
2025-11-13 11:51:04 +0100

    Create ClockHandler class, move time-based handling to this class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using static SDL2.SDL;
+﻿using static SDL2.SDL;
 
 namespace CHIP_8;
 
@@ -17,19 +16,13 @@ public static class Program
         /// Configure VM components
         CPU          cpu      = new();
         RenderEngine renderer = new(64, 32, 8);
+        ClockHandler clock    = new();
         AudioEngine  _        = new(() => cpu.SoundTimer, v => cpu.SoundTimer = v);
         InputHandler input    = new(
             () => cpu.WaitingForKeyPress,
             k  => cpu.KeyPressed(k),
             () => cpu.Keyboard,
             v  => cpu.Keyboard = v);
-        
-        /// Confifure program timers
-        var  frameTimer  = Stopwatch.StartNew();         // Timer for display out
-        long frameTicks  = Stopwatch.Frequency / 60;     // running at 60Hz
-        var  cpuTimer    = Stopwatch.StartNew();         // Timer for the CPU clock
-        long cpuTicks    = Stopwatch.Frequency / 700;    // running at 700Hz
-        long accumulator = 0;                            // Used to keep display synced
         
         /// Read the program into memory
         using (BinaryReader reader = new(new FileStream("../../../ROMs/BRIX", FileMode.Open)))
@@ -45,27 +38,9 @@ public static class Program
         
         while (input.Running)
         {
-            if (!cpu.WaitingForKeyPress && cpuTimer.ElapsedTicks >= cpuTicks)
-            {
-                cpu.Step();
-                cpuTimer.Restart();
-            }
-            
-            // Accumulate for a stable 60Hz
-            long elapsed = frameTimer.ElapsedTicks;
-            frameTimer.Restart();
-            accumulator += elapsed;
-
-            // Handle input outside the 60Hz rate
-            input.PollEvent();
-            
-            /// Output to display at exactly 60Hz cadence
-            while (accumulator >= frameTicks)
-            {
-                renderer.Render(cpu.Display);
-
-                accumulator -= frameTicks;
-            }
+            clock.StepCPU(cpu.Step);                                // Step the CPU
+            input.PollEvent();                                      // Handle input outside the CPU & Frame timers
+            clock.StepFrame(() => renderer.Render(cpu.Display));    // Draw a frame to the display
         }
 
         renderer.Dispose();
--- src/ClockHandler.cs
+++ src/ClockHandler.cs
@@ -1,6 +1,38 @@
+using System.Diagnostics;
+
 namespace CHIP_8;
 
 public class ClockHandler
 {
+    private const int CPU_Hz   = 700;   // Frequency the CPU runs at
+    private const int Frame_Hz = 60;    // Frequency the display refreshes at
     
+    private long accumulator;           // Used to keep display & CPU clocks synced
+    
+    private readonly Stopwatch _cpuTimer   = Stopwatch.StartNew();           // Timer for the CPU clock
+    private readonly long      _cpuTicks   = Stopwatch.Frequency / CPU_Hz;   // running at CPU_Hz
+    private readonly Stopwatch _frameTimer = Stopwatch.StartNew();           // Timer for display out
+    private readonly long      _frameTicks = Stopwatch.Frequency / Frame_Hz; // running at Frame_Hz
+    
+
+    public void StepCPU(Action step)
+    {
+        if (_cpuTimer.ElapsedTicks >= _cpuTicks)
+        {
+            step();
+            _cpuTimer.Restart();
+        }
+    }
+
+    public void StepFrame(Action render)
+    {
+        long elapsed = _frameTimer.ElapsedTicks;
+        _frameTimer.Restart();
+        accumulator += elapsed;
+        while (accumulator >= _frameTicks)
+        {
+            render();
+            accumulator -= _frameTicks;
+        }
+    }
 }
