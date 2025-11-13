bd00afe6716b46196681752f9f2fb5d814447731
c3hmke <cehmke91@gmail.com>
2025-11-13 14:24:00 +0100

    Add super simple ROM selection

--- Program.cs
+++ Program.cs
@@ -8,6 +8,29 @@ public static class Program
 {
     private static void Main(string[] args)
     {
+        /// --------------------------------------------------------------------
+        ///     ROM selection menu (console-based, before SDL is initialized)
+        /// --------------------------------------------------------------------
+        // Load the ROMs in the provided directory
+        string ROMDir = Path.GetFullPath("../../../ROMs");
+        string[] ROMs = Directory.GetFiles(ROMDir)
+            .OrderBy(Path.GetFileNameWithoutExtension)
+            .ToArray();
+            
+        if (ROMs.Length == 0) throw new Exception("No ROMs found!");
+        
+        Console.WriteLine($"ROMs:");
+        for (var i = 0; i < ROMs.Length; i++)
+            Console.WriteLine($"[{i}] {Path.GetFileName(ROMs[i])}");
+        
+        Console.WriteLine("Select ROM index: ");
+        string line = Console.ReadLine()!;
+
+        string ROMPath = ROMs[int.Parse(line)];
+        
+        // --------------------------------------------------------------------
+        //      SDL and emulator initialization
+        // --------------------------------------------------------------------
         const uint flags = SDL_INIT_VIDEO |
                            SDL_INIT_AUDIO |
                            SDL_INIT_TIMER |
@@ -27,10 +50,11 @@ public static class Program
             v  => cpu.Keyboard = v);
         
         /// Load a ROM into the program memory
-        string romPath = args.Length > 0 ? args[0] : "../../../ROMs/BRIX";
-        cpu.LoadProgram(File.ReadAllBytes(romPath));
+        cpu.LoadProgram(File.ReadAllBytes(ROMPath));
         
-        /// Program loop
+        // --------------------------------------------------------------------
+        //      Main program loop
+        // --------------------------------------------------------------------
         while (input.Running)
         {
             clock.Tick();
